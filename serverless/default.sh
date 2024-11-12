a3e891daa59f7a75c9c5796a25eb51cd64ff4b4d
dscapes <n.v.pushkin@gmail.com>
2024-11-12 20:47:19 +0400

    Create default.sh

---
+++ serverless/default.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This file will be sourced in init.sh
+# Namespace functions with provisioning_
+
+# https://raw.githubusercontent.com/ai-dock/stable-diffusion-webui/main/config/provisioning/default.sh
+
+pip install runpod
