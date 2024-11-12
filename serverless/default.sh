359ed04adb6eef80627427525cbf17ac98d9b687
dscapes <n.v.pushkin@gmail.com>
2024-11-12 20:49:41 +0400

    Update default.sh

--- serverless/default.sh
+++ serverless/default.sh
@@ -4,4 +4,11 @@
 
 # https://raw.githubusercontent.com/ai-dock/stable-diffusion-webui/main/config/provisioning/default.sh
 
+# Установка пакета runpod
 pip install runpod
+
+# Загрузка файла serverless-api.py с GitHub
+curl -o serverless-api.py https://raw.githubusercontent.com/dscapes/runpod/refs/heads/main/serverless/serverless-api.py
+
+# Запуск serverless-api.py
+python serverless-api.py
